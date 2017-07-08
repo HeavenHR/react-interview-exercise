77bd47ae0216c683ed1ef181192d7de830dd79ad
mark <markshapiro12345@gmail.com>
2017-07-08 23:35:53 +0200

    added paginator

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,32 +1,33 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './FriendList.css';
 import FriendListItem from './FriendListItem';
+import { map } from 'lodash';
 
 class FriendList extends Component {
-  render () {
-    return (
-      <ul className={styles.friendList}>
-        {
-          this.props.friends.map((friend, index) => {
-            return (
-              <FriendListItem
-                key={index}
-                id={index}
-                name={friend.name}
-                starred={friend.starred}
-                {...this.props.actions} />
-            );
-          })
-        }
-      </ul>
-    );
-  }
+    render () {
+        return (
+            <ul className={styles.friendList}>
+                {
+                    map(this.props.friends, (friend, index) => {
+                        return (
+                            <FriendListItem
+                                key={index}
+                                id={Number(index)}
+                                name={friend.name}
+                                starred={friend.starred}
+                                {...this.props.actions} />
+                        );
+                    })
+                }
+            </ul>
+        );
+    }
 
 }
 
 FriendList.propTypes = {
-  friends: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+    friends: PropTypes.object.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
 export default FriendList;
--- src/containers/FriendListApp.js
+++ src/containers/FriendListApp.js
@@ -3,35 +3,43 @@ import styles from './FriendListApp.css';
 import { connect } from 'react-redux';
 
 import {addFriend, deleteFriend, starFriend} from '../actions/FriendsActions';
-import { FriendList, AddFriendInput } from '../components';
+import { setPage } from '../actions/PaginationActions';
+import { FriendList, AddFriendInput, Paginator } from '../components';
+
+import {mapValues} from 'lodash';
 
 class FriendListApp extends Component {
 
-  render () {
-    const { friendlist: { friendsById }} = this.props;
-
-    const actions = {
-      addFriend: this.props.addFriend,
-      deleteFriend: this.props.deleteFriend,
-      starFriend: this.props.starFriend
-    };
-
-    return (
-      <div className={styles.friendListApp}>
-        <h1>The FriendList</h1>
-        <AddFriendInput addFriend={actions.addFriend} />
-        <FriendList friends={friendsById} actions={actions} />
-      </div>
-    );
-  }
+    render () {
+        const { friendlist: { friendsById }, pagination: { currentPageNo, currentPageIds, pageSize } } = this.props;
+        const actions = {
+            addFriend: this.props.addFriend,
+            deleteFriend: this.props.deleteFriend,
+            starFriend: this.props.starFriend,
+            setPage: this.props.setPage
+        };
+        const list = mapValues(currentPageIds, id => friendsById[id]);
+        return (
+            <div className={styles.friendListApp}>
+              <h1>The FriendList</h1>
+              <AddFriendInput addFriend={actions.addFriend} />
+              <FriendList friends={list} actions={actions} />
+              <Paginator setPage={actions.setPage}
+                         length={friendsById.length}
+                         currentPageNo={currentPageNo}
+                         pageSize={pageSize}/>
+            </div>
+        );
+    }
 }
 
 function mapStateToProps(state) {
-  return state
+    return state
 }
 
 export default connect(mapStateToProps, {
-  addFriend,
-  deleteFriend,
-  starFriend
+    addFriend,
+    deleteFriend,
+    starFriend,
+    setPage
 })(FriendListApp)
